447cdeb12d5b5a3d37e1284d6483c8b448688a92
aelnagar1412-rgb <aelnagar1412@gmail.com>
2026-01-16 16:42:21 +0200

    Update main.cjs

--- main.cjs
+++ main.cjs
@@ -1,21 +1,16 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      preload: path.join(__dirname, "preload.js")
     }
-  })
+  });
 
-  win.loadFile(path.join(__dirname, 'dist', 'index.html'))
+  win.loadFile("dist/index.html");
 }
 
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.whenReady().then(createWindow);
