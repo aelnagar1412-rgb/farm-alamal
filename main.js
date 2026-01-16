d046b7635e18660d9fcbde11f9937b30eac9f5a1
aelnagar1412-rgb <aelnagar1412@gmail.com>
2026-01-16 15:29:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog } = require('electron')
 const path = require('path')
 
+let mainWindow
+let splash
+
+function createSplash() {
+  splash = new BrowserWindow({
+    width: 400,
+    height: 300,
+    frame: false,
+    alwaysOnTop: true,
+    transparent: true
+  })
+
+  splash.loadFile(path.join(__dirname, 'splash.html'))
+}
+
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    show: false,
+    fullscreen: true,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -12,6 +30,21 @@ function createWindow() {
   })
 
   mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'))
+
+  mainWindow.once('ready-to-show', () => {
+    splash.close()
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.on('did-fail-load', () => {
+    dialog.showErrorBox(
+      "خطأ في تشغيل البرنامج",
+      "الواجهة لم يتم تحميلها. تأكد من ملفات البرنامج."
+    )
+  })
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createSplash()
+  createWindow()
+})
