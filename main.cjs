79a43992d3bb2ed9ce0c3b9e79acc4776f871165
aelnagar1412-rgb <aelnagar1412@gmail.com>
2026-01-16 20:56:41 +0200

    Update main.cjs

--- main.cjs
+++ main.cjs
@@ -6,11 +6,12 @@ function createWindow() {
     width: 1200,
     height: 800,
     webPreferences: {
-      contextIsolation: true
+      contextIsolation: true,
+      nodeIntegration: false
     }
   });
 
-  win.loadFile(path.join(__dirname, "dist", "index.html"));
+  win.loadFile(path.join(__dirname, "dist/index.html"));
 }
 
 app.whenReady().then(createWindow);
