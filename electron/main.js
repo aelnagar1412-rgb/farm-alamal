b13d88b609677cc3970a31d2bdfb5a00632d6075
aelnagar1412-rgb <aelnagar1412@gmail.com>
2026-01-16 13:45:34 +0200

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -3,11 +3,8 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1300,
-    height: 800,
-    webPreferences: {
-      contextIsolation: true
-    }
+    width: 1200,
+    height: 800
   });
 
   win.loadFile(path.join(__dirname, "../dist/index.html"));
